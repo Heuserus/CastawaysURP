45bdb5bfd5ec4d317793b1f5fb26abee8895f8df
Ben Häußermann <ben.haussermann@gmail.com>
2023-01-04 12:20:09 +0100

    map is 3d now

--- Assets/Scripts/MapGenerator.cs
+++ Assets/Scripts/MapGenerator.cs
@@ -3,7 +3,7 @@ using System.Collections;
 
 public class MapGenerator : MonoBehaviour {
 
-	public enum DrawMode {NoiseMap, ColourMap};
+	public enum DrawMode {NoiseMap, ColourMap, Mesh};
 	public DrawMode drawMode;
 
 	public int mapWidth;
@@ -17,6 +17,8 @@ public class MapGenerator : MonoBehaviour {
 
 	public int seed;
 	public Vector2 offset;
+
+	public float meshHeightMultiplier;
 
 	public bool autoUpdate;
 
@@ -40,9 +42,11 @@ public class MapGenerator : MonoBehaviour {
 
 		MapDisplay display = FindObjectOfType<MapDisplay> ();
 		if (drawMode == DrawMode.NoiseMap) {
-			display.DrawTexture (TextureGenerator.TextureFromHeightMap(noiseMap));
+			display.DrawTexture (TextureGenerator.TextureFromHeightMap (noiseMap));
 		} else if (drawMode == DrawMode.ColourMap) {
-			display.DrawTexture (TextureGenerator.TextureFromColourMap(colourMap, mapWidth, mapHeight));
+			display.DrawTexture (TextureGenerator.TextureFromColourMap (colourMap, mapWidth, mapHeight));
+		} else if (drawMode == DrawMode.Mesh) {
+			display.DrawMesh (MeshGenerator.GenerateTerrainMesh (noiseMap,meshHeightMultiplier), TextureGenerator.TextureFromColourMap (colourMap, mapWidth, mapHeight));
 		}
 	}
 
--- Assets/Scripts/TextureGenerator.cs
+++ Assets/Scripts/TextureGenerator.cs
@@ -1,18 +1,20 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
+using System.Collections;
 
-public static class TextureGenerator
-{
-    public static Texture2D TextureFromColourMap(Color[] colourMap, int width, int height){
-        Texture2D texture = new Texture2D (width, height);
-        texture.SetPixels(colourMap);
-        texture.Apply();
-        return texture;
-    }
+public static class TextureGenerator {
 
-    public static Texture2D TextureFromHeightMap(float[,] heightMap){
-        int width = heightMap.GetLength (0);
+	public static Texture2D TextureFromColourMap(Color[] colourMap, int width, int height) {
+		Texture2D texture = new Texture2D (width, height);
+		texture.filterMode = FilterMode.Point;
+		texture.wrapMode = TextureWrapMode.Clamp;
+		texture.SetPixels (colourMap);
+		texture.Apply ();
+		return texture;
+	}
+
+
+	public static Texture2D TextureFromHeightMap(float[,] heightMap) {
+		int width = heightMap.GetLength (0);
 		int height = heightMap.GetLength (1);
 
 		Color[] colourMap = new Color[width * height];
@@ -21,8 +23,8 @@ public static class TextureGenerator
 				colourMap [y * width + x] = Color.Lerp (Color.black, Color.white, heightMap [x, y]);
 			}
 		}
-		return TextureFromColourMap(colourMap, width,height);
-    }
 
-    
+		return TextureFromColourMap (colourMap, width, height);
+	}
+
 }
