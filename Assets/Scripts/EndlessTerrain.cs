b9d69745f3d78a7ba7ac5114334afe289f08cd4a
Ben Häußermann <ben.haussermann@gmail.com>
2023-01-05 18:31:51 +0100

    almost at infinite generation

---
+++ Assets/Scripts/EndlessTerrain.cs
@@ -0,0 +1,116 @@
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class EndlessTerrain : MonoBehaviour {
+
+	public const float maxViewDst = 250;
+	public Transform viewer;
+    public static MapGenerator mapGenerator;
+
+	public static Vector2 viewerPosition;
+    public GameObject waterPlane;
+    public Material mapMaterial;
+	int chunkSize;
+	int chunksVisibleInViewDst;
+
+	Dictionary<Vector2, TerrainChunk> terrainChunkDictionary = new Dictionary<Vector2, TerrainChunk>();
+	List<TerrainChunk> terrainChunksVisibleLastUpdate = new List<TerrainChunk>();
+
+	void Start() {
+        mapGenerator = FindObjectOfType<MapGenerator>();
+		chunkSize = MapGenerator.mapChunkSize - 1;
+		chunksVisibleInViewDst = Mathf.RoundToInt(maxViewDst / chunkSize);
+	}
+
+	void Update() {
+		viewerPosition = new Vector2 (viewer.position.x, viewer.position.z);
+		UpdateVisibleChunks ();
+	}
+		
+	void UpdateVisibleChunks() {
+
+		for (int i = 0; i < terrainChunksVisibleLastUpdate.Count; i++) {
+			terrainChunksVisibleLastUpdate [i].SetVisible (false);
+		}
+		terrainChunksVisibleLastUpdate.Clear ();
+			
+		int currentChunkCoordX = Mathf.RoundToInt (viewerPosition.x / chunkSize);
+		int currentChunkCoordY = Mathf.RoundToInt (viewerPosition.y / chunkSize);
+
+		for (int yOffset = -chunksVisibleInViewDst; yOffset <= chunksVisibleInViewDst; yOffset++) {
+			for (int xOffset = -chunksVisibleInViewDst; xOffset <= chunksVisibleInViewDst; xOffset++) {
+				Vector2 viewedChunkCoord = new Vector2 (currentChunkCoordX + xOffset, currentChunkCoordY + yOffset);
+
+				if (terrainChunkDictionary.ContainsKey (viewedChunkCoord)) {
+					terrainChunkDictionary [viewedChunkCoord].UpdateTerrainChunk ();
+					if (terrainChunkDictionary [viewedChunkCoord].IsVisible ()) {
+						terrainChunksVisibleLastUpdate.Add (terrainChunkDictionary [viewedChunkCoord]);
+					}
+				} else {
+					terrainChunkDictionary.Add (viewedChunkCoord, new TerrainChunk (viewedChunkCoord, chunkSize, transform, waterPlane,mapMaterial));
+				}
+
+			}
+		}
+	}
+
+	public class TerrainChunk {
+
+        
+        
+		GameObject meshObject;
+        GameObject waterPlane;
+		Vector2 position;
+		Bounds bounds;
+        MeshRenderer meshRenderer;
+        MeshFilter meshFilter;
+
+        
+
+        public TerrainChunk(Vector2 coord, int size, Transform parent, GameObject waterPlanePrefab, Material material) {
+			position = coord * size;
+			bounds = new Bounds(position,Vector2.one * size);
+			Vector3 positionV3 = new Vector3(position.x,0,position.y);
+
+			meshObject = new GameObject("Terrain Chunk");
+            meshRenderer = meshObject.AddComponent<MeshRenderer>();
+            meshFilter = meshObject.AddComponent<MeshFilter>();
+            meshRenderer.material = material;
+			meshObject.transform.position = positionV3;
+			meshObject.transform.parent = parent;
+            
+            waterPlane = Instantiate(waterPlanePrefab);
+            waterPlane.transform.position = positionV3;
+			waterPlane.transform.position += new Vector3(0f,14f,0f);
+			waterPlane.transform.parent = parent;
+			SetVisible(false);
+
+            mapGenerator.RequestMapData(OnMapDataReceived);
+		}
+
+        void OnMapDataReceived(MapData mapData){
+            mapGenerator.RequestMeshData(mapData,OnMeshDataReceived);
+        }
+
+        void OnMeshDataReceived(MeshData meshData){
+            meshFilter.mesh = meshData.CreateMesh();
+        }
+
+		public void UpdateTerrainChunk() {
+			float viewerDstFromNearestEdge = Mathf.Sqrt(bounds.SqrDistance (viewerPosition));
+			bool visible = viewerDstFromNearestEdge <= maxViewDst;
+			SetVisible (visible);
+		}
+
+		public void SetVisible(bool visible) {
+			meshObject.SetActive (visible);
+            waterPlane.SetActive (visible);
+		}
+
+		public bool IsVisible() {
+			return meshObject.activeSelf;
+		}
+
+	}
+}
